3fb2be6439b9e32af07251e5fd9affee76730bf5
kuhpferd <uli.toll@codingcow.de>
2022-06-17 12:22:42 +0100

    fixed import function

--- checker/scraper/management/commands/import_old_checker_db.py
+++ checker/scraper/management/commands/import_old_checker_db.py
@@ -5,42 +5,43 @@ import sqlite3 as db
 from sqlite3 import DatabaseError
 
 class Command(BaseCommand):  
+    def add_arguments(self, parser):
+        parser.add_argument('db_file_path', nargs='+', type=str)
        
         
     def handle(self, *args, **options):
         """ 
             imports from a legacy database
             needas a filename as parameter"""
-        
-        db_file =args[0]                    
-        self.import_db(db_file)
-        connection = None
-        try:
-            connection = db.connect(db_file)
-            print(f"Database ready. SQLite {db.version}")
-            print("Checking data ...")
-            cursor= connection.cursor()
-            response = cursor.execute("SELECT * FROM user")            
-            for data in response:
-                name = data[0]
-                email = data[1]
-                flag = data[2]
-                if sub:= Subscription.objects.filter(name=name, email=email)[:1][0]:
-                    return
-                sub = Subscription.objects.create(name=name, email=email, flag=flag)
-                sub.save()
-            print('All users migrated.')
+        for db_file in options['db_file_path']:        
+            
+            connection = None
+            try:
+                connection = db.connect(db_file)
+                print(f"Database ready. SQLite {db.version}")
+                print("Checking data ...")
+                cursor= connection.cursor()
+                response = cursor.execute("SELECT * FROM user")            
+                for data in response:
+                    name = data[0]
+                    email = data[1]
+                    flag = data[2]
+                   # if sub:= Subscription.objects.filter(name=name, email=email)[:1][0]:
+                    #    return
+                    sub = Subscription.objects.create(name=name, email=email, flag=flag)
+                    sub.save()
+                print('All users migrated.')
 
-        except DatabaseError as e:
-            if f"{e}" == "no such table: user":
-                print("No data found.")                
-            else:
-                print(f"Error: {e}")
-        finally:
-            if connection:                
-                connection.close()
-                print("Connection closed.")
-    
+            except DatabaseError as e:
+                if f"{e}" == "no such table: user":
+                    print("No data found.")                
+                else:
+                    print(f"Error: {e}")
+            finally:
+                if connection:                
+                    connection.close()
+                    print("Connection closed.")
+        
         
                 
             
